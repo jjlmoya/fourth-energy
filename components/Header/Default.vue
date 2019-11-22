1ff1eb17c04bfa2c36bf0264870db3c01f242148
jjlmoya <juanjo1989@gmail.com>
2019-11-22 15:24:37 +0100

    Initial Commit

--- components/Header/Default.vue
+++ components/Header/Default.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="header header--default">
         <div class="header__logo">
-            <img src="/cuartaevolucion.png" alt="cuartaevolucion">
+            <img src="./cuartaevolucion.png" alt="cuartaevolucion">
         </div>
     </header>
 </template>
